f0f45d2ceaed80f90c3913676732816756803a8d
Elena Zherdeva <hollyhellen@yandex.ru>
2019-03-12 13:26:49 +0300

    Initial Commit

---
+++ Telegram_charts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telegram_charts
+//
+//  Created by Zherdeva Elena on 12/03/2019.
+//  Copyright © 2019 Zherdeva Elena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Telegram_chartsTests/Telegram_chartsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Telegram_chartsTests.swift
+//  Telegram_chartsTests
+//
+//  Created by Zherdeva Elena on 12/03/2019.
+//  Copyright © 2019 Zherdeva Elena. All rights reserved.
+//
+
+import XCTest
+@testable import Telegram_charts
+
+class Telegram_chartsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
