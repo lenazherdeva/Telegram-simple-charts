b74542efc8136198e1363d270675cd177b61939a
Elena Zherdeva <helenzherdeva@gmail.com>
2019-03-15 13:07:27 +0300

    unable to plot graph

--- Telegram_charts/ChartView.swift
+++ Telegram_charts/ChartView.swift
@@ -8,28 +8,54 @@
 
 import Foundation
 import UIKit
-
-
-class Chart: UIView {
+ 
+ 
+ struct DataEntry {
+    let value: Int
+    let label: String
+}
+ 
+ extension DataEntry: Comparable {
+     static func <(lhs: DataEntry, rhs: DataEntry) -> Bool {
+        return lhs.value < rhs.value
+    }
+     static func ==(lhs: DataEntry, rhs: DataEntry) -> Bool {
+        return lhs.value == rhs.value
+     }
+}
+ 
+ 
+ class Chart: UIView {
+ 
+    // contain dataLayer and pointsLayer
+    private let mainLayer: CALayer = CALayer()
     
-    // layer with main line
+    // layer with main graph==line
     private let dataLayer: CALayer = CALayer()
     
+    private let scrollView: UIScrollView = UIScrollView()
+    
     // horizontal lines layer
-    private let gridLayer: CALayer = CALayer()
+    //private let gridLayer: CALayer = CALayer()
+    
+    let lineGap: CGFloat = 60.0
+    let topSpace: CGFloat = 40.0
+    let bottomSpace: CGFloat = 40.0
+    /// The top most horizontal line in the chart will be 10% higher than the highest value in the chart
+    let topHorizontalLine: CGFloat = 110.0 / 100.0
+    
     
     // points on a dataLayer
+    //private var dataPoints: [CGPoint]?
+    
+    // input data points
+    var inputData: [DataEntry]?
     private var dataPoints: [CGPoint]?
     
-    private let points: [CGPoint] = [
-        CGPoint(x: 0, y: 1),
-        CGPoint(x: 12, y: 3),
-        CGPoint(x: 5, y: 4),
-        CGPoint(x: 8, y: 11),
-        CGPoint(x: 7, y: 2)
-        ].sorted { $0.x < $1.x }
     
+
     private var shapeLayer = CAShapeLayer()
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -37,26 +63,66 @@ class Chart: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.drawChart()
-    
+        print("required_init")
+        setupView()
     }
+    
+     private func setupView() {
+        mainLayer.addSublayer(dataLayer)
+        scrollView.layer.addSublayer(mainLayer)
+        self.addSubview(scrollView)
+        print("setupView")
+    }
+    
+    override func layoutSubviews() {
+        print(inputData)
+        scrollView.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
+        scrollView.contentSize = CGSize(width: CGFloat(inputData!.count) * lineGap, height: self.frame.size.height)
+        mainLayer.frame = CGRect(x: 0, y: 0, width: CGFloat(inputData!.count) * lineGap, height: self.frame.size.height)
+        dataLayer.frame = CGRect(x: 0, y: topSpace, width: mainLayer.frame.width, height: mainLayer.frame.height - topSpace - bottomSpace)
+        
+        dataPoints = convertDataToPoints(data: inputData!)
+        print("layoutSubviews")
+        
+        print(dataPoints)
+        
+        self.drawChart()
+    }
+    
+
+    
+    private func convertDataToPoints(data: [DataEntry])-> [CGPoint] {
+        print("convertData")
+        if let max = data.max()?.value, let min = data.min()?.value {
+            var result: [CGPoint] = []
+            let minMaxRange: CGFloat = CGFloat(max - min) * topHorizontalLine
+            
+            for i in 0..<data.count {
+                let height = dataLayer.frame.height * (1 - ((CGFloat(data[i].value) - CGFloat(min)) / minMaxRange))
+                let point = CGPoint(x: CGFloat(i)*lineGap + 40, y: height)
+                result.append(point)
+            }
+            return result
+        }
+        return []
+    }
+    
     
     private func drawChart() {
+
         let aPath = UIBezierPath()
-        aPath.move(to: CGPoint(x: 0, y: 0))
+        aPath.move(to: dataPoints![0])
         
-        let randomInt = CGFloat.random(in: 5..<20)
-        
-        for point in points {
-            aPath.addLine(to: CGPoint(x: point.x * 20, y: point.y * randomInt))
+        for i in 1..<dataPoints!.count {
+            aPath.addLine(to: dataPoints![i])
         }
         shapeLayer.path = aPath.cgPath
-
+        shapeLayer.strokeColor = UIColor.black.cgColor
+        shapeLayer.fillColor = UIColor.clear.cgColor
         
+        layer.addSublayer(shapeLayer)
     }
-    
-    
-    
-    
-    
 }
+
+
+
--- Telegram_charts/ViewController.swift
+++ Telegram_charts/ViewController.swift
@@ -15,9 +15,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ChartView.inputData = getData()
+        //print(ChartView.inputData)
+        print("viewDidLoad")
     }
+    
+    private func getData()-> [DataEntry] {
+        var result: [DataEntry] = []
+        for i in 0..<30 {
+            let value = Int(arc4random() % 100)
+            let formatter = DateFormatter()
+            formatter.dateFormat = "d MMM"
+            var date = Date()
+            date.addTimeInterval(TimeInterval(24*60*60*i))
+            
+            result.append(DataEntry(value: value, label: formatter.string(from: date)))
+        }
+        //print("RESULT", result)
+        return result
+    }
+    
+}
 
-
+private func getDataFromFile()-> [DataEntry] {
+    var result: [DataEntry] = []
+    if let path = Bundle.main.path(forResource: "chart_data", ofType: "json") {
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let person = jsonResult["person"] as? [Any] {
+                // do stuff
+            }
+        } catch {
+            // handle error
+        }
+    }
+    
+    
+    return result
 }
 
